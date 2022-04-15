d00279378bb4f3696be3efac93b7b8dc69866852
chaechae <kimyeongchaee@gmail.com>
2022-04-15 22:44:55 +0900

    4week commit

--- src/main/java/com/example/practice/MainActivity.kt
+++ src/main/java/com/example/practice/MainActivity.kt
@@ -15,13 +15,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val song = Song(binding.mainMiniplayerTitleTv.text.toString(),binding.mainMiniplayerSingerTv.text.toString())
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(),binding.mainMiniplayerSingerTv.text.toString(),0,60,false)
 
         binding.mainPlayerCl.setOnClickListener{
             //startActivity(Intent(this,SongActivity::class.java))
             val intent = Intent(this, SongActivity::class.java)
             intent.putExtra("title", song.title)
             intent.putExtra("singer", song.singer)
+            intent.putExtra("second", song.second)
+            intent.putExtra("playTime", song.playTime)
+            intent.putExtra("isPlaying", song.isPlaying)
             startActivity(intent)
         }
         initBottomNavigation()
